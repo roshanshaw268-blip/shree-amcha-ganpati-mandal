cd67edd5c563066698efdd1bbecb646480f8620c
roshanshaw268-blip <roshanshaw268@gmail.com>
2026-08-07 14:40:10 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,6 @@
 const supabaseUrl = "https://oqlgbmjcfrvagilkawkp.supabase.co";
-const supabaseKey = "YOUR_PUBLISHABLE_KEY";
+
+const supabaseKey = "sb_publishable_jo_Kv1BCUr0mxoY6FcAnrw_9GEumP1z";
 
 window.sb = window.supabase.createClient(
   supabaseUrl,
