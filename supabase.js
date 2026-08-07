9548cddb2405174a79a8b9d82ba908e4de084502
roshanshaw268-blip <roshanshaw268@gmail.com>
2026-08-07 08:22:36 +0530

    Initialize Supabase client connection

--- supabase.js
+++ supabase.js
@@ -1,1 +1,13 @@
+// Import Supabase
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+// Project URL
+const SUPABASE_URL = "https://oqlgbmjcfrvagilkawkp.supabase.co";
+
+// Publishable Key
+const SUPABASE_KEY = "sb_publishable_jo_Kv1BCUr0mxoY6FcAnrw_9GEumP1z";
+
+// Connect
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+console.log("Supabase Connected");
